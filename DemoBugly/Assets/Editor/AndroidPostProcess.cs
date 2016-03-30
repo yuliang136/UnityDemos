7707a401490738db2f247ac8726c29b4595f3328
yuliang <yuliang@naocy.com>
2016-03-30 14:40:44 +0800

    Bugly Demo提交
    Bugly Demo提交

---
+++ DemoBugly/Assets/Editor/AndroidPostProcess.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.XCodeEditor;
+#endif
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class AndroidPostProcess {
+
+	#if UNITY_EDITOR
+	[PostProcessBuild(999)]
+	public static void OnPostProcessBuild( BuildTarget target, string pathToBuiltProject )
+	{
+		
+		if (target != BuildTarget.Android) {
+			Debug.LogWarning("Target is not Android. AndroidPostProcess will not run");
+			return;
+		}
+
+		string unityVersion = Application.unityVersion;
+		Log ("" + unityVersion);
+		if (unityVersion.StartsWith ("5.")) {
+			// do nothing
+		} else { // 4.x
+			DoSomethingU4X(pathToBuiltProject);
+		}
+	}
+	#endif
+
+	public static void Log(string message)
+	{
+		UnityEngine.Debug.Log("Android PostProcess: "+message);
+	}
+
+	private static void DoSomethingU4X(string pathToBuiltProject){
+
+		string rootBuglyPluginPath = Path.Combine(Application.dataPath, "Plugins/BuglyPlugins");
+
+		if (Directory.Exists(rootBuglyPluginPath)) {
+			string androidLibsPath = Path.Combine(rootBuglyPluginPath, "Android/libs");
+
+			if (Directory.Exists(androidLibsPath)) {
+
+				string targetProjectPath = Path.Combine(pathToBuiltProject, PlayerSettings.productName);
+				if (Directory.Exists(targetProjectPath)) {
+					CopyDirectory(androidLibsPath, Path.Combine(targetProjectPath, "libs"));
+				}
+			}
+		}
+	}
+
+	private static void CopyDirectory(string sourcePath, string destinationPath)
+	{
+		DirectoryInfo info = new DirectoryInfo(sourcePath);
+		Directory.CreateDirectory(destinationPath);
+		foreach (FileSystemInfo  fsi in info.GetFileSystemInfos())
+		{
+			if (fsi.Name.EndsWith(".meta")) {
+				continue;
+			}
+			string destName = Path.Combine(destinationPath, fsi.Name);
+			if (fsi is System.IO.FileInfo) {
+				File.Copy(fsi.FullName, destName);
+			}      
+			else                     
+			{
+				Directory.CreateDirectory(destName);
+				CopyDirectory(fsi.FullName, destName);
+			}
+		}
+	}
+}
---
+++ DemoBugly/Assets/Scripts/TestBugly.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class TestBugly : MonoBehaviour {
+
+    private const string BuglyAppIDForAndroid = "900023762";
+
+    public void Start()
+    {
+        BuglyAgent.PrintLog(LogSeverity.LogInfo, "Demo Start()");
+
+        InitBuglySDK();
+
+        // 代码自动抛出一次.
+        OutException();
+    }
+
+    public void OutException()
+    {
+        //throwException(new System.Exception("Test Ronyl Exception！！！"));
+
+        FindGameObject();
+    }
+
+    private void FindGameObject()
+    {
+        //System.Console.Write("it will throw NullReferenceException");
+        Debug.Log("It will throw NullReferenceException");
+
+        GameObject go = GameObject.Find("test");
+        string gName = go.name;
+
+        //System.Console.Write(gName);
+        Debug.Log(gName);
+    }
+
+    //private void throwException(Exception e)
+    //{
+    //    if (e == null)
+    //        return;
+
+    //    BuglyAgent.PrintLog(LogSeverity.LogWarning, "Throw exception: {0}", e.ToString());
+
+    //    testDeepFrame(e);
+    //}
+
+    //private void testDeepFrame(Exception e)
+    //{
+
+    //    throw e;
+    //}
+
+    void InitBuglySDK()
+    {
+
+        // TODO NOT Required. Enable debug log print, please set false for release version
+        BuglyAgent.ConfigDebugMode(true);
+
+        // TODO NOT Required. Register log callback with 'BuglyAgent.LogCallbackDelegate' to replace the 'Application.RegisterLogCallback(Application.LogCallback)'
+        // BuglyAgent.RegisterLogCallback (CallbackDelegate.Instance.OnApplicationLogCallbackHandler);
+
+        // 如何获得设备唯一Id号.
+        // 用设备号代替用户名.
+        
+        BuglyAgent.ConfigDefault("Bugly", null, "ronylTest", 0);
+
+#if UNITY_IPHONE || UNITY_IOS
+		BuglyAgent.InitWithAppId (BuglyAppIDForiOS);
+#elif UNITY_ANDROID
+        BuglyAgent.InitWithAppId(BuglyAppIDForAndroid);
+#endif
+
+        // TODO Required. If you do not need call 'InitWithAppId(string)' to initialize the sdk(may be you has initialized the sdk it associated Android or iOS project),
+        // please call this method to enable c# exception handler only.
+        BuglyAgent.EnableExceptionHandler();
+
+        // TODO NOT Required. If you need to report extra data with exception, you can set the extra handler
+        // BuglyAgent.SetLogCallbackExtrasHandler (MyLogCallbackExtrasHandler);
+
+    }
+}
