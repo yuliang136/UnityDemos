c0075d670a1c5fa37773cd1f969251f8f813a77e
yuliang <yuliang@naocy.com>
2016-04-13 11:20:59 +0800

    Demo MouseDown
    Demo MouseDown

---
+++ DemoMouseDown/Assets/Scripts/CheckMouseDown.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckMouseDown : MonoBehaviour 
+{
+
+
+    // OnMouseEnter is called when the mouse entered the GUIElement or Collider
+    public void OnMouseEnter()
+    {
+        Debug.Log("Mouse Down");
+    }
+}
