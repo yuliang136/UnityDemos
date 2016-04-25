1ccacbec4d43bf74d785d63e5e336ffdcb9d92b0
yuliang <yuliang@naocy.com>
2016-04-25 16:08:56 +0800

    DeviceUniID
    DeviceUniID

---
+++ DeviceUniID/Assets/Scripts/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    string strDeviceId = SystemInfo.deviceUniqueIdentifier;
+
+        Debug.Log(strDeviceId);
+	}
+}
