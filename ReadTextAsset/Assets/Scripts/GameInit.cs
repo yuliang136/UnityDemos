b2904a4aad27d59a71b6c218befb7681aaeeff8f
yuliang <yuliang@naocy.com>
2016-05-07 15:04:20 +0800

    使用TextAsset读取二进制文件
    使用TextAsset读取二进制文件

---
+++ ReadTextAsset/Assets/Scripts/GameInit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using ProtoBuf;
+using schemes;
+
+public class GameInit : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	    TextAsset ta = Resources.Load<TextAsset>("ProtoBytes/CharacterName");
+
+        MemoryStream ms = new MemoryStream(ta.bytes);
+
+        CharacterName data = Serializer.Deserialize<schemes.CharacterName>(ms);
+
+        string strShowReadInfo = string.Format(
+                                        "ReadInfo {0}",
+                                        data.items.Count);
+
+        Debug.Log(data.items[0].woman);
+
+        Debug.Log(strShowReadInfo);
+	}
+	
+}
