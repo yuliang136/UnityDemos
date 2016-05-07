de8c04fd477a13d56da178de6ea406eaf5be820e
yuliang <yuliang@naocy.com>
2016-05-07 15:47:02 +0800

    尝试使用泛型加载数据
    尝试使用泛型加载数据

--- ReadTextAsset/Assets/Scripts/GameInit.cs
+++ ReadTextAsset/Assets/Scripts/GameInit.cs
@@ -10,19 +10,36 @@ public class GameInit : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-	    TextAsset ta = Resources.Load<TextAsset>("ProtoBytes/CharacterName");
+        //TextAsset ta = Resources.Load<TextAsset>("ProtoBytes/CharacterName");
 
-        MemoryStream ms = new MemoryStream(ta.bytes);
+        //MemoryStream ms = new MemoryStream(ta.bytes);
 
-        CharacterName data = Serializer.Deserialize<schemes.CharacterName>(ms);
+        //CharacterName data = Serializer.Deserialize<schemes.CharacterName>(ms);
 
-        string strShowReadInfo = string.Format(
-                                        "ReadInfo {0}",
-                                        data.items.Count);
+        //string strShowReadInfo = string.Format(
+        //                                "ReadInfo {0}",
+        //                                data.items.Count);
+
+        //Debug.Log(data.items[0].woman);
+
+	    CharacterName data = LoadBinaryData<schemes.CharacterName>("ProtoBytes/CharacterName");
 
         Debug.Log(data.items[0].woman);
 
-        Debug.Log(strShowReadInfo);
+	    // 尝试用泛型函数加载数据.
+
 	}
-	
+
+    public T LoadBinaryData<T>(string strPath)
+    {
+        TextAsset ta = Resources.Load<TextAsset>(strPath);
+
+        MemoryStream ms = new MemoryStream(ta.bytes);
+
+        T data = Serializer.Deserialize<T>(ms);
+
+        return data;
+
+    }
+
 }
